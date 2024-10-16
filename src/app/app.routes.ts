247e93a83ecd520620174477fd3d552032db8792
Efren Ortiz <efrendoa.24@gmail.com>
2024-10-15 18:04:41 -0600

    not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { ViewProductsComponent } from './components/view-products/view-products.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     {path: "products-list", component: ProductsListComponent},
     {path: "view-products", component: ViewProductsComponent},
+    {path: "not-found", component: NotFoundComponent},
     {path: "", redirectTo: "products-list", pathMatch: "full"},
-    {path: "**", redirectTo: "products-list", pathMatch: "full"}
+    {path: "**", redirectTo: "not-found", pathMatch: "full"}
 ];
